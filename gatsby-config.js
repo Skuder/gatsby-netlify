507327d7ae28d48c50942d0d7ac46e6a9ded8be4
Eduardo <eduardo@Eduardos-Mac.local>
2020-04-22 17:48:52 -0700

    pwa

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,6 +33,18 @@ module.exports = {
         hostingWPCOM: false,
       },
     },
-    `gatsby-plugin-react-helmet`
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS Bootstrap WP Tutorial`,
+        start_url: `/`,
+        background_color: `#f8f9fa`,
+        theme_color: `#FFFFFF`,
+        display: `standalone`,
+      },
+    },
+    `gatsby-plugin-offline`
   ],
 }
